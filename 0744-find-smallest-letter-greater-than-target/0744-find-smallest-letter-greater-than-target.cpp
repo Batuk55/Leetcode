203d8635f8e02d4be810bfe21cbae554f73b057c
Batuk Singh <batukthakur5501@gmail.com>
2026-01-31 23:35:15 +0530

    Time: 0 ms (100%), Space: 19.9 MB (24.67%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,28 @@
+/*class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(int i = 0; i < letters.size(); i++){
+            if(letters[i] > target){
+                return letters[i];
+            }
+        }
+        //wrap around case
+        return letters[0];
+    }
+};
+*/
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for (int i = 0; i < letters.size(); i++) {
+            if (letters[i] <= target) {
+                continue;
+            } else {
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+};
