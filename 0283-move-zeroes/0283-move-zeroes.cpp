e9156ad49846a638901a8987838015c0c5be163c
Batuk Singh <batukthakur5501@gmail.com>
2026-03-01 19:59:54 +0530

    Time: 3 ms (15.5%), Space: 23.8 MB (55.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        /*
         int a = 0;
         for(int i = 0; i<nums.size(); i++)
         {
@@ -9,6 +10,17 @@ public:
                 swap(nums[i], nums[a]);
                 a++;
             }
+        }*/
+        
+        int i = 0;
+        for(int j = 0; j<nums.size(); j++){
+            if(nums[j]!=0){
+                nums[i] = nums[j];
+                i++;               
+            }
+        }
+        for(int k = i; k < nums.size(); k++){
+            nums[k] = 0;
         }
     }
 };
