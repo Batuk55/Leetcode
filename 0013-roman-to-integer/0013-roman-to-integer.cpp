53e47223130467d66a4079138afb6a234541b528
Batuk Singh <batukthakur5501@gmail.com>
2026-03-18 03:45:38 +0530

    Time: 11 ms (25.99%), Space: 12.9 MB (58.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     int romanToInt(string s) {
-      unordered_map<char, int> val = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
 
-        int sum= 0;
-        for(int i= 0; i <s.size()-1; i++){
-            if(val[s[i]]< val[s[i+1]]){
-            sum = sum - val[s[i]];}
-        else {
-            sum = sum+val[s[i]];
+        unordered_map<char, int> mpp;
+        mpp = {{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+               {'C', 100}, {'D', 500}, {'M', 1000}};
+
+        int sum = mpp[s[s.size() - 1]];
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (mpp[s[i]] < mpp[s[i + 1]]) {
+                // subtract
+                sum = sum - mpp[s[i]];
+            } else {
+                // add
+                sum = sum + mpp[s[i]];
+            }
         }
-        }  
-        //adding last character
-        sum = sum + val[s[s.size()-1]];
+        // sum = sum + mpp[s[s.size() - 1]];
         return sum;
-
     }
 };
-
-/*class Solution {
-public:
-    int romanToInt(string s) {
-        int I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
-        int sum = 0;
-        for (int i = 0; i < s.size()-1; i++)
-            if (s[i] < s[i + 1]) {
-                // subtract
-                sum = sum - s[i];
-            } else {
-                //add
-                sum = sum + s[i];
-            }
-    return sum;}
-};*/
