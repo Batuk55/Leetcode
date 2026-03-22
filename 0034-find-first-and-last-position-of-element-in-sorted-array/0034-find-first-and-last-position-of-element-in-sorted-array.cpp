2ca1ad7f5fee86d7c965fdbfdd268b4c54383259
Batuk Singh <batukthakur5501@gmail.com>
2026-03-22 23:57:05 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,96 @@
-class Solution{
+class Solution {
 public:
-    vector<int> searchRange(vector<int> &nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        // O(log n) solution
         int n = nums.size();
+        int low = 0, high = n - 1;
+
+        int first = -1;
+        while (high >= low) { // for first occurance
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+
+            if (target < nums[mid])
+                high = mid - 1;
+            else if (target > nums[mid])
+                low = mid + 1;
+        }
+
+        if(first == -1){
+            return {-1,-1};
+        }
+
+        low = 0, high = n - 1;
+
+        int last = -1;
+        while (high >= low) { // for last occurance
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            }
+            if (target < nums[mid]) {
+                high = mid - 1;
+            } else if (target > nums[mid])
+                low = mid + 1;
+        }
+        return {first, last};
+    }
+};
+
+/* O(n) solution 1
+int n = nums.size();
+
+int i = 0;
+while( i< n){
+    if(nums[i]==target) break;
+    i++;
+}
+
+int j = n-1;
+while( j >= 0){
+    if(nums[j] == target) break;
+    j--;
+}
+
+if(i == n || j == -1){
+    return {-1,-1};
+}
+return {i,j};
+*/
+
+/* // O(n) solution 2
+
+        int n = nums.size();
+        int first = -1, last = -1;
 
         int i = 0;
         while( i< n){
-            if(nums[i]==target) break;
+            if(nums[i]==target){
+                first = i;
+                 break;
+                 }
             i++;
         }
 
         int j = n-1;
         while( j >= 0){
-            if(nums[j] == target) break;
+            if(nums[j] == target){
+                last = j;
+                break;
+            }
             j--;
         }
 
-        if(i == n || j == -1){
-            return {-1,-1};
-        }
-        return {i,j};
-    }
-};
+        // if(i == n || j == -1){
+        //     return {-1,-1};
+        // }
+
+        return {first , last};
+        */
