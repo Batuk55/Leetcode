5dfb70f6de8a3430a5386f14a858103d4324d8b7
Batuk Singh <batukthakur5501@gmail.com>
2026-03-27 00:09:00 +0530

    Time: 0 ms (100%), Space: 12.5 MB (71.81%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int low = 1, high = nums.size() - 2;
+        if(nums.size() == 1) return 0;
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[high + 1] > nums[high])
+            return high + 1;
+
+        while (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            // now how to eliminat left or right halfs??
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else  {
+                high = mid - 1;
+            }
+        }
+        return -1; // never happens
+    }
+};
