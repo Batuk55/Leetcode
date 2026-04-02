82124bcceafabac7666ce22c912fff9fdbd7b383
Batuk Singh <batukthakur5501@gmail.com>
2026-04-02 20:21:08 +0530

    Time: 3 ms (95.03%), Space: 22.9 MB (77.3%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    long long totalHours(vector<int>& piles, int speed) {
-        long long hours = 0;
+    long long total_hours(vector<int>& piles, int k) {
+        long long hoursum = 0;
         for (int pile : piles) {
-            hours += (pile + speed - 1) / speed; // ceil
+            hoursum += (pile + k - 1) / k;
         }
-        return hours;
+        return hoursum;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
+
         int low = 1, high = *max_element(piles.begin(), piles.end());
-        int ans = high;
+        // int k = INT_MAX; // or k = high;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (totalHours(piles, mid) <= h) {
-                ans = mid;
-                high = mid - 1;   // try smaller speed
-            } else {
-                low = mid + 1;    // need faster speed
+            long long hours = total_hours(piles , mid);
+            if(hours <= h){
+                // k = mid;
+                high = mid - 1;
+            }
+            else if(hours > h){
+                low = mid + 1;
             }
         }
-
-        return ans;
+        return low;
     }
 };
+/*
+class Solution {
+public:
+    long long total_hours(vector<int>& piles, int k) {
+        long long hoursum = 0;
+        for (int pile : piles) {
+            hoursum += (pile + k - 1) / k;
+        }
+        return hoursum;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int k = 1;
+        while (true) {
+
+            long long hoursum = total_hours(piles, k);
+            // for (int pile : piles) {
+            //     hoursum += (pile + k - 1) / k;
+            // }
+
+            if (hoursum <= h)
+                return k;
+            k++;
+        }
+        return 0;
+    }
+};
+*/
