4f158069b3f6329baed6eff44f93c6db7534f4aa
Batuk Singh <batukthakur5501@gmail.com>
2026-04-16 00:51:36 +0530

    Time: 2 ms (50.59%), Space: 9.8 MB (24.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> mapS, mapT;
+        for (int i = 0; i < s.size(); i++) {
+            mapS[s[i]]++;
+            mapT[t[i]]++;
+        }
+        return (mapS == mapT);
+        // if(mapS == mapT) return true;
+        // return false;
+    }
+};
+/*
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(t.begin(), t.end());
+        sort(s.begin(), s.end());
+        return s==t;
+    }
+};
+/*
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
         map<char, int> mpp;
         for (int i = 0; i< s.size(); i++) {
             mpp[s[i]]++;
@@ -15,3 +40,6 @@ return mpp==mapp;
 return false;
     }
 };
+/
+//or
+*/
