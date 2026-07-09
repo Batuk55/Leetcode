8c36dc032e83b7f8953c25bf1ce9155e49af567e
Batuk Singh <batukthakur5501@gmail.com>
2026-07-09 10:05:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.31%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,13 +3,16 @@ public:
     void helper(vector<vector<int>> &ans, vector<int>& nums, int k, int n, vector<int> &arr, int i, long current_sum){
 
         if(current_sum > n || arr.size() > k) return;
+        if(current_sum== n && arr.size()== k){
+            ans.push_back(arr);
+            return;
+        }
         if(i == nums.size()){
-            if(arr.size() == k && current_sum == n) ans.push_back(arr);
             return;
         }
         //include
         arr.push_back(nums[i]);
-        helper(ans, nums, k, n, arr, i+1, current_sum + nums[i]);
+        helper(ans, nums, k, n, arr, i+1, current_sum + nums[i]); // used running sum(in recursion) we could have used a seperate sumofarr function in each recursive step as well, but increased SC due to increased O(k) sum calculation each time.
         arr.pop_back();
 
         //not-include
