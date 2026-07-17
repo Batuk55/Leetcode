1718ca84a3600b3aa90976a60eee2d7cebf7049f
Batuk Singh <batukthakur5501@gmail.com>
2026-07-17 15:54:24 +0530

    Time: 118 ms (6.67%), Space: 193.6 MB (14.16%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+    int MOD = (int)1e9+7;
     vector<int> find_nse(vector<int>& arr){
         int n = arr.size();
         vector<int> ans(n);
         stack<int> st;
         int i = n-1;
         while(i>=0){
-            while(!st.empty() && arr[i]<=arr[st.top()]) st.pop();
+            while(!st.empty() && arr[i]<arr[st.top()]) st.pop();
             ans[i] = st.empty() ? n : st.top();
             st.push(i--);
         }
@@ -18,9 +19,9 @@ public:
         stack<int> st;
         int i = 0;
         while(i<n){
-            while(!st.empty() && arr[i]<arr[st.top()]) st.pop();
+            while(!st.empty() && arr[i]<=arr[st.top()]) st.pop();
             ans[i] = st.empty() ? -1 : st.top();
-            st.push(i++ );
+            st.push(i++);
         }
         return ans;
     }
@@ -28,16 +29,15 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
         vector<int> nse = find_nse(arr);
         vector<int> pseOrEq = find_psee(arr);
+        
         long long sum = 0;
-        int MOD = (int)1e9+7;
-
+        
         for(int i = 0; i < arr.size(); i++){
             int left = i - pseOrEq[i];
             int right =nse[i] - i;
 
-            sum = (sum + (right*left*1LL* arr[i])%MOD)%MOD;
+            sum = (sum + (right*left*1LL* arr[i]) % MOD) % MOD;
         }
         return sum;
-        
     }
 };
