e9b3499a875766b730c802d6b4d6903bc061c1c4
Batuk Singh <batukthakur5501@gmail.com>
2026-08-09 20:48:42 +0530

    Time: 0 ms (100%), Space: 18 MB (90.46%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size();
-        int n = image[0].size();
+    int drow[4] = {-1, 0, 1, 0};
+    int dcol[4] = {0, 1, 0, -1};
+    void  dfs(vector<vector<int>>& image, int row, int col, int &color, int &startColor){
+        image[row][col] = color;
+        
+        for(int i = 0; i<4; i++){
+            int newr = row + drow[i];
+            int newc = col + dcol[i];
 
-        vector<vector<int>> visited = image;
-        queue<pair<int,int>> q;
-
-        q.push({sr, sc});
-        visited[sr][sc] = color;
-
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-
-        while(!q.empty()){
-            auto curr = q.front();
-            q.pop();
-            
-            int r = curr.first;
-            int c = curr.second;
-
-            for(int i = 0; i<4; i++){
-                int newr = r + drow[i];
-                int newc = c + dcol[i];
-
-                if(newr>=0 && newr <m && newc>=0 && newc < n && 
-                visited[newr][newc] != color && image[sr][sc] == image[newr][newc]){
-                    q.push({newr, newc});
-                    visited[newr][newc] = color;
-                }
+            if(newr>=0 && newr < image.size() && newc>=0 && newc < image[0].size() && 
+            image[newr][newc] != color &&  image[newr][newc] == startColor){ //image[sr][sc] == image[newr][newc]
+                // q.push({newr, newc});
+                dfs(image, newr, newc, color, startColor);                
             }
         }
-        
-        return visited;  
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // vector<vector<int>> ans = image;
+        int startColor = image[sr][sc];
+
+        dfs(image, sr, sc, color, startColor);
+        // return ans;
+        return image;
         
     }
 };
