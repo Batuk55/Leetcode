f1fd3977710b3eeb51d633bc0b2a7608941a294f
Batuk Singh <batukthakur5501@gmail.com>
2026-09-13 12:50:17 +0530

    Time: 67 ms (70.67%), Space: 113.5 MB (11.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]]++;
+        unordered_set<int> st;
+        for(auto i : nums){
+            st.insert(i);
         }
-        // for(map<int,int> :: iterator it = nums.begin(); it !=nums.end() ;
-        // it++) or
-        for (auto it : nums) {
-            if (mpp[it] >= 2) {
-                return true;
-            }
-        }
-        return false;
+        return !(st.size() == nums.size());
     }
 };
