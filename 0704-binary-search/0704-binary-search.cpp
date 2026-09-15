9fb57c72514479175d491005565944aff6a01e2e
Batuk Singh <batukthakur5501@gmail.com>
2026-09-15 09:16:15 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i = 0; i< nums.size() ; i++){
-            if(nums[i]== target){
-                return i;
-                break;
-            }
+    int binSearch(vector<int>& nums, int low, int high, int target){
+        if(low>high) return -1;// base case
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == target) return mid;
 
-        }
+        if(nums[mid]>target) return binSearch(nums, low, mid-1, target);
+        if(nums[mid]<target) return binSearch(nums, mid+1, high, target);
         return -1;
+        
+    }
+    int search(vector<int>& nums, int target) {
+        
+        return binSearch(nums, 0, nums.size()-1, target);
+        
     }
 };
