3f21b21780c197518341e016160fd3811dc5f4e7
Batuk Singh <batukthakur5501@gmail.com>
2026-09-21 20:05:59 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.86%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int binSearch(vector<int>& nums, int low, int high, int target){
-        if(low>high) return -1;// base case
-        int mid = low + (high - low) / 2;
-        if(nums[mid] == target) return mid;
+    // int binSearch(vector<int>& nums, int low, int high, int target){
+    //     if(low>high) return -1;// base case
+    //     int mid = low + (high - low) / 2;
+    //     if(nums[mid] == target) return mid;
 
-        if(nums[mid]>target) return binSearch(nums, low, mid-1, target);
-        if(nums[mid]<target) return binSearch(nums, mid+1, high, target);
-        return -1;
+    //     if(nums[mid]>target) return binSearch(nums, low, mid-1, target);
+    //     if(nums[mid]<target) return binSearch(nums, mid+1, high, target);
+    //     return -1;
         
-    }
+    // }
     int search(vector<int>& nums, int target) {
         
-        return binSearch(nums, 0, nums.size()-1, target);
-        
+        // return binSearch(nums, 0, nums.size()-1, target);
+         int low = 0, high = nums.size()-1;
+
+        while(low<= high){
+            int mid = low + (high - low) /2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[mid] < target)  low= mid+1;
+            else high = mid-1;
+        }
+        return -1;
     }
 };
