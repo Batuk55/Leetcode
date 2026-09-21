dd50faf1c34c6f59cb824a8fe20fafa72ed0db95
Batuk Singh <batukthakur5501@gmail.com>
2026-09-21 22:07:45 +0530

    Time: 0 ms (100%), Space: 19.7 MB (61.65%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,28 +1,16 @@
-/*class Solution {
-public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        for(int i = 0; i < letters.size(); i++){
-            if(letters[i] > target){
-                return letters[i];
-            }
-        }
-        //wrap around case
-        return letters[0];
-    }
-};
-*/
-
-
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for (int i = 0; i < letters.size(); i++) {
-            if (letters[i] <= target) {
-                continue;
-            } else {
-                return letters[i];
-            }
+        int low = 0, high = letters.size()-1;
+
+        while(low<= high){
+            int mid = low + (high - low) / 2;
+
+            if(letters[mid] > target) high = mid-1;
+            else low = mid+1;
+
         }
-        return letters[0];
+        if(low>= letters.size()) return letters[0];
+        return letters[low];
     }
 };
